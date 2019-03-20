da36ebbf2bb5e52d0090bba10bb52a42595043b8
yangzhou <angenal@hotmail.com>
2019-03-20 09:23:34 +0800

    up scheduler worker

--- go-program/go-scheduler/worker.go
+++ go-program/go-scheduler/worker.go
@@ -13,9 +13,16 @@ import (
 )
 
 // 计划任务执行器
-// Make a Worker Pool
 var WorkerPool *work.WorkerPool
+
+// 任务执行器的配置
+var workerPoolOpts work.WorkerPoolOptions
+
+// 任务的配置
 var workerConfig WorkerConfig
+
+// 任务的上下文对象
+var workerCtx Context
 
 // 初始化配置
 func init() {
@@ -49,16 +56,22 @@ func init() {
 			return redis.Dial("tcp", cliAddr, cliOpt)
 		},
 	}
-	// Worker Config
+
+	// 任务的配置
 	workerConfig = WorkerConfig{
-		concurrency:     1,
+		concurrency:     1000, // 并发数,默认0不限制
 		namespace:       "work",
 		pool:            cliPool,
 		sleepBackoffs:   []int64{},
 		log:             true, // 开启当前上下文的日志输出Context.log
-		manual:          true, // 是否为 手动执行任务
 		drainOnShutdown: true, // 计划任务 close 时, 清理队列
 	}
+
+	// 任务执行器的配置
+	workerPoolOpts = work.WorkerPoolOptions{}
+
+	// 任务的上下文对象
+	workerCtx = Context{customer: fmt.Sprintf("C%d", time.Now().Year())}
 }
 
 // 初始化 Worker Pool
@@ -67,14 +80,12 @@ func initWorkerPool() {
 		return
 	}
 
-	// 自动化任务上下文对象
-	c := Context{customer: fmt.Sprintf("C%d", time.Now().Year())}
-	workerPoolOpts := work.WorkerPoolOptions{}
-	WorkerPool = work.NewWorkerPoolWithOptions(c, workerConfig.concurrency, workerConfig.namespace, workerConfig.pool, workerPoolOpts)
+	// 任务的上下文对象
+	WorkerPool = work.NewWorkerPoolWithOptions(workerCtx, workerConfig.concurrency, workerConfig.namespace, workerConfig.pool, workerPoolOpts)
 
-	// 任务执行时通用过滤器 Add middleware that will be executed for each job
-	WorkerPool.Middleware(c.Log)
-	WorkerPool.Middleware(c.FindCustomer)
+	// 任务执行时,通用过滤器 Add middleware that will be executed for each job
+	WorkerPool.Middleware(workerCtx.Log)               // 日志跟踪
+	WorkerPool.Middleware(workerCtx.FindCtxUseJobAttr) // 通过查找参数补充任务的上下文信息
 }
 
 // 初始化 Worker Jobs
@@ -84,10 +95,10 @@ func initWorkerJobs() {
 	}
 
 	// 添加任务/发送邮件 Map the name of jobs to handler functions
-	WorkerPool.Job("send_email", (*Context).SendEmail)
+	WorkerPool.Job("send_email", workerCtx.SendEmail)
 
-	// 添加任务/导出数据 Customize options
-	WorkerPool.JobWithOptions("export", work.JobOptions{Priority: 10, MaxFails: 1, MaxConcurrency: 1}, (*Context).Export)
+	// 添加并发任务/导出数据 Customize options
+	WorkerPool.JobWithOptions("export", work.JobOptions{Priority: 10, MaxFails: 0, MaxConcurrency: workerConfig.concurrency}, workerCtx.Export)
 }
 
 // 计划任务 close
@@ -113,9 +124,9 @@ func TestWorker() {
 	defer WorkerPool.Start()
 
 	/**
-	计划执行任务 Worker Jobs
+	定期执行任务 Worker Jobs
 	*/
-	if workerConfig.manual == false {
+	if workerCtx.manual == false {
 		// 每1秒  参考(spec: 秒/分/时/日/月/星期) https://godoc.org/github.com/robfig/cron
 		WorkerPool.PeriodicallyEnqueue("*/1 * * * * *", "send_email")
 		// 每1小时 @hourly
@@ -125,7 +136,7 @@ func TestWorker() {
 	/**
 	手动执行任务 Worker Jobs
 	*/
-	if workerConfig.manual == true {
+	if workerCtx.manual == true {
 		enqueuer := work.NewEnqueuer(workerConfig.namespace, workerConfig.pool)
 		// 添加任务/发送邮件
 		q, id := work.Q{"address": "123456.qq.com", "subject": "some message"}, UniqueWorkQ()
@@ -138,42 +149,22 @@ func TestWorker() {
 	}
 }
 
-// 任务配置项 Worker Config
-type WorkerConfig struct {
-	concurrency     uint
-	namespace       string // eg, "myapp-work"
-	pool            *redis.Pool
-	sleepBackoffs   []int64
-	log             bool // 开启当前上下文的日志输出Context.log
-	manual          bool // 是否为 手动执行任务
-	drainOnShutdown bool // 计划任务 close 时, 清理队列
-}
-
-// 任务上下文对象 Worker Context
-type Context struct {
-	customer string
-	manual   bool // 是否为 手动执行任务
-}
-
-func (c *Context) log(job *work.Job, method string) {
-	if workerConfig.log {
-		log.Printf(" job[%s]:%s \n  %+v \n  %+v \n", job.Name, method, c, job)
-	}
-}
-
+// 日志跟踪
 func (c *Context) Log(job *work.Job, next work.NextMiddlewareFunc) error {
 	//c.log(job, "Log")
 	return next()
 }
 
-func (c *Context) FindCustomer(job *work.Job, next work.NextMiddlewareFunc) error {
+// 通过查找参数补充任务的上下文信息
+func (c *Context) FindCtxUseJobAttr(job *work.Job, next work.NextMiddlewareFunc) error {
 	if e := c.Check(job); e != nil {
 		return e
 	}
-	c.log(job, "FindCustomer")
+	//c.log(job, "FindCustomer")
 	return next()
 }
 
+// 任务：发送邮件
 func (c *Context) SendEmail(job *work.Job) error {
 	if e := c.Check(job); e != nil {
 		return e
@@ -193,37 +184,64 @@ func (c *Context) SendEmail(job *work.Job) error {
 
 		// SendEmailTo(addr, subject)
 		log.Printf(" job[%s]:Work End manual. \n  %+v \n  address: %s\n  subject: %s \n", job.Name, c, address, subject)
-
+		c.Checkin(job)
 		return nil
 	}
 
 	// Auto Checkin with no args, Worker Pool
 	log.Printf(" job[%s]:Work End one time. \n  %+v \n ", job.Name, c)
-
+	c.Checkin(job)
 	return nil
 }
 
+// 任务：导出功能
 func (c *Context) Export(job *work.Job) error {
-	job.Checkin(fmt.Sprintf("customer=%s:export", c.customer))
+	c.Checkin(job)
 	return nil
+}
+
+// 任务配置项 Worker Config
+type WorkerConfig struct {
+	concurrency     uint
+	namespace       string // eg, "myapp-work"
+	pool            *redis.Pool
+	sleepBackoffs   []int64
+	log             bool // 开启当前上下文的日志输出Context.log
+	drainOnShutdown bool // 计划任务 close 时, 清理队列
+}
+
+// 任务上下文对象 Worker Context
+type Context struct {
+	customer string // 客户
+	manual   bool   // 是否为 手动执行任务 run method: cron or manual
+}
+
+func (c *Context) log(job *work.Job, method string) {
+	if workerConfig.log {
+		log.Printf(" job[%s]:%s \n  %+v \n  %+v \n", job.Name, method, c, job)
+	}
 }
 
 func (c *Context) Check(job *work.Job) error {
 	// If there's a customer param, set it in the context for future middleware and handlers to use.
-	c.manual = workerConfig.manual
 	if _, ok := job.Args["customer"]; ok {
 		c.customer = job.ArgString("customer")
 		if err := job.ArgError(); err != nil {
 			return err
 		}
-	} else {
-		if job.Args == nil {
-			job.Args = work.Q{"customer": c.customer}
-		} else {
-			job.Args["customer"] = c.customer
-		}
+	}
+	// run method: cron or manual
+	if _, ok := job.Args["cron"]; ok {
+		c.manual = false
+	}
+	if _, ok := job.Args["manual"]; ok {
+		c.manual = true
 	}
 	return nil
+}
+
+func (c *Context) Checkin(job *work.Job) {
+	job.Checkin(fmt.Sprintf("job[%s]%s:#%s", c.customer, job.Name, job.ID))
 }
 
 func UniqueWorkQ() work.Q {
