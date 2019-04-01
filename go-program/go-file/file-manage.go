70c83df6b2c67999583b507cb6ba420f9de8157a
yangzhou <angenal@hotmail.com>
2019-04-01 12:04:50 +0800

    up file manage code

--- go-program/go-file/file-manage.go
+++ go-program/go-file/file-manage.go
@@ -4,22 +4,28 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"time"
+	"path/filepath"
+
+	"github.com/rs/xid"
 )
 
 // 文件管理：创建文件
 func TestCreateFile() {
 	fmt.Println("-------------------------\n文件管理：创建文件")
 
-	p, _ := ioutil.TempDir(os.TempDir(), "")
-	fmt.Println("  tmpdir:", p)
-
-	fName := fmt.Sprintf("%s\\~%d.tmp", os.TempDir(), time.Now().Unix())
-	f, e := os.Create(fName)
+	tmpDir, e := ioutil.TempDir(os.TempDir(), "")
 	if e != nil {
-		fmt.Fprintf(os.Stderr, "  创建tmp文件异常：%+v", e)
+		fmt.Fprintf(os.Stderr, "  创建tmp目录异常：%v", e)
+		panic(e) // 无法创建目录时，进程退出code:2
+	}
+
+	f, e := os.Create(filepath.Join(tmpDir, fmt.Sprintf("_%s.tmp", xid.New().String()[12:])))
+	//f, e := ioutil.TempFile("", "*.tmp") // dir = os.TempDir() 等同于上面>创建tmp文件
+	if e != nil {
+		fmt.Fprintf(os.Stderr, "  创建tmp文件异常：%v", e)
 		panic(e) // 无法创建文件时，进程退出code:2
 	}
+	defer os.RemoveAll(tmpDir) // 不删除也可以，因为系统会定期清理
 
 	fmt.Printf("  创建tmp文件成功：%s \n", f.Name())
 	var fileContent = "文件内容\r\n内容..."
