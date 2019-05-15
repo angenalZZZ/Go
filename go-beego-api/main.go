a1e1fc73540f9471de6da9a05d13d9fefc969cdd
yangzhou <angenal@hotmail.com>
2019-05-15 17:55:02 +0800

    up beego api

--- go-beego-api/main.go
+++ go-beego-api/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	_ "github.com/angenalZZZ/Go/go-beego-api/routers"
+	"github.com/astaxie/beego"
 	"github.com/astaxie/beego/orm"
 
-	"github.com/astaxie/beego"
+	//_ "github.com/lib/pq"
+	//_ "github.com/mattn/go-sqlite3"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func init() {
-	_ = orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("sqlconn"))
+	_ = orm.RegisterDataBase("default", "mysql", beego.AppConfig.String("mysqlconn"))
+	//o := orm.NewOrm() // 默认使用别名为default的数据库
+	//_ = o.Using("default") // Ormer interface
+	DbFirst_Bee_Generate() // Db First
+}
+
+// Db First 从数据库生成 models、routers、controllers
+// > bee generate go-beego-api [-tables=""] [-driver=mysql] [-conn="root:123456@tcp(127.0.0.1:3306)/AppAuth?charset=utf8"] [-level=3]
+func DbFirst_Bee_Generate() {
 }
 
 func main() {
---
+++ go-beego-api/models/xo_db_test.go
@@ -0,0 +1,90 @@
+package models
+
+import (
+	"encoding/hex"
+	"github.com/astaxie/beego"
+	"github.com/google/uuid"
+	"github.com/xormplus/xorm"
+	"testing"
+	"time"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+// 1. 生成数据库实体models - use DbFirst tool - https://github.com/xo/dburl
+//  > xo mysql://root:123456@127.0.0.1:3306/AppAuth?parseTime=true -o ./models
+//  > xo mssql://sa:Your_password123@localhost:1434/AppAuth?parseTime=true -o ./models
+
+// 2. 数据库引擎增强版 xorm db engine
+var db *xorm.Engine
+
+func init() {
+	var err error
+	if err = beego.LoadAppConfig("ini", "../conf/app.conf"); err != nil {
+		panic(err)
+	}
+
+	sqlconn := beego.AppConfig.String("mssqlconn")
+
+	// 原版方式创建引擎
+	//db, err = xorm.NewEngine("mssql", sqlconn)
+	// 也可以针对特定数据库快捷创建
+	//db, err = xorm.NewPostgreSQL(sqlconn)
+	//db, err = xorm.NewSqlite3(sqlconn)
+
+	// 数据库连接异常
+	if err != nil {
+		panic(sqlconn + " -> " + err.Error())
+	}
+}
+
+func UUID32() string {
+	var dst [32]byte
+	id := uuid.New()
+	hex.Encode(dst[:], id[:4])
+	hex.Encode(dst[8:12], id[4:6])
+	hex.Encode(dst[12:16], id[6:8])
+	hex.Encode(dst[16:20], id[8:10])
+	hex.Encode(dst[20:], id[10:])
+	return string(dst[:])
+}
+
+func TestAddUser(t *testing.T) {
+
+	user1, err := db.Transaction(func(session *xorm.Session) (i interface{}, e error) {
+		user1 := Authuser{
+			ID:          UUID32(),
+			Code:        "xxx",
+			Name:        "xxx",
+			Password:    "",
+			Salt:        "",
+			Avatar:      "",
+			Orgid:       "",
+			Email:       "",
+			Phone:       "",
+			Status:      "",
+			Revision:    0,
+			Createdby:   "admin",
+			Createdtime: time.Now(),
+			Updatedby:   "admin",
+			Updatedtime: time.Now(),
+		}
+		if _, err := session.Insert(&user1); err != nil {
+			return nil, err
+		}
+		user2 := Authuser{Name: "yyy"}
+		if _, err := session.Where("ID = ?", user1.ID).Update(&user2); err != nil {
+			return nil, err
+		}
+		if _, err := session.Exec("delete from AuthUser where Name = ?", user2.Name); err != nil {
+			return nil, err
+		}
+		return user1, nil
+	})
+
+	if err != nil {
+		t.Fatal(err)
+	} else {
+		t.Logf("TestAddUser: %v", user1)
+	}
+}
