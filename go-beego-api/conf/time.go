6eb5284a7c6194f0f1518c9bd3276d542d4cc448
Angenal <angenal@hotmail.com>
2019-05-20 10:30:01 +0800

    Create time.go

---
+++ go-beego-api/conf/time.go
@@ -0,0 +1,23 @@
+package conf
+
+import "time"
+
+/*
+前面是含义, 后面是 go 的表示值(多种表示用逗号","分割)
+年　 06,2006
+月份 1,01,Jan,January
+日　 2,02,_2
+时　 3,03,15,PM,pm,AM,am
+分　 4,04
+秒　 5,05
+周几 Mon,Monday
+时区时差表示 -07,-0700,Z0700,Z07:00,-07:00,MST
+时区字母缩写 MST
+以上所有的值都唯一对应一个时间部分,并且涵盖了很多格式组合
+*/
+// 时间格式化字符串
+const Dateformat string = "2006-01-02"
+const DateTimeformat string = "2006-01-02 15:04:05"
+
+// 中国时区
+var ChinaTimeLocation, _ = time.LoadLocation("Asia/Chongqing")
