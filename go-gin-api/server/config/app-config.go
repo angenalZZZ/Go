e0f0e3a08e791191950c397aef497f636f0fccf3
yangzhou <angenal@hotmail.com>
2019-06-23 17:35:55 +0800

    up gin app config

--- go-gin-api/server/config/app-config.go
+++ go-gin-api/server/config/app-config.go
@@ -4,21 +4,23 @@ import (
 	"github.com/angenalZZZ/Go/go-gin-api/pkg/config"
 	"github.com/angenalZZZ/Go/go-gin-api/pkg/config/app"
 	"github.com/lexkong/log"
+	"github.com/spf13/pflag"
 )
 
 // App配置
-var AppConfig *app.Config
+var (
+	AppConfig     *app.Config
+	AppConfigFile = pflag.StringP("config", "c", "config.yml", "api server config file path.")
+)
 
 // 初始化:App配置
 func init() {
 	if AppConfig == nil {
 		AppConfig = new(app.Config)
-		// 配置文件
-		files := []string{"config.yml", "/etc/app/config.yml"}
-		// 运行环境
+		// 环境配置
 		cfg := config.Config{Environ: &config.Environ{EnvironmentPrefix: "API"}}
 		// 解析配置
-		if err := cfg.Load(AppConfig, files...); err != nil {
+		if err := cfg.Load(AppConfig, *AppConfigFile); err != nil {
 			panic(err)
 		}
 		// 日志跟踪
