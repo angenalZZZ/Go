16bfe48a1d69725dafa35d4ebe76d0e4e5f35eb3
yangzhou <angenal@hotmail.com>
2020-02-23 16:17:56 +0800

    update app config

--- go-gin-api/go.mod
+++ go-gin-api/go.mod
@@ -1,3 +1,9 @@
 module github.com/angenalZZZ/Go/go-gin-api
 
 go 1.13
+
+require (
+	github.com/angenalZZZ/Go/pkg v0.0.0-20200223080101-5d2cbb143e73 // indirect
+	github.com/gin-gonic/gin v1.5.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+)
--- go-gin-api/server/config/app-config.go
+++ go-gin-api/server/config/app-config.go
@@ -1,31 +1,27 @@
 package config
 
 import (
-	"github.com/angenalZZZ/Go/go-gin-api/pkg/config"
-	"github.com/angenalZZZ/Go/go-gin-api/pkg/config/app"
+	"github.com/angenalZZZ/Go/pkg"
+	"github.com/angenalZZZ/Go/pkg/config"
 	"github.com/lexkong/log"
 	"github.com/spf13/pflag"
 )
 
 // App配置
 var (
-	AppConfig     *app.Config
-	AppConfigFile = pflag.StringP("config", "c", "config.yml", "api server config file path.")
+	AppConfig     *Config
+	AppConfigFile = pflag.StringP("config", "c", "app.config.yml", "api server config file path.")
 )
 
-// 初始化:App配置
+// init 初始化:App配置
 func init() {
 	if AppConfig == nil {
-		AppConfig = new(app.Config)
+		AppConfig = new(Config)
 		// 环境配置
 		cfg := config.Config{Environ: &config.Environ{EnvironmentPrefix: "API"}}
 		// 解析配置
-		if err := cfg.Load(AppConfig, *AppConfigFile); err != nil {
-			panic(err)
-		}
+		pkg.MustNotError(cfg.Load(AppConfig, *AppConfigFile))
 		// 日志跟踪
-		if err := log.InitWithConfig(&AppConfig.Log); err != nil {
-			panic(err)
-		}
+		pkg.MustNotError(log.InitWithConfig(&AppConfig.Log))
 	}
 }
